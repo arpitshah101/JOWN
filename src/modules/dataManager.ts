bcd6878f8b9459a896042d6af28338c86e8b9e81
Jon <jjgetahun@gmail.com>
2016-12-07 23:12:42 -0500

    Fixed the structure of the functions in dataManager.ts.

--- src/modules/dataManager.ts
+++ src/modules/dataManager.ts
@@ -1,46 +1,61 @@
 // promise-bluebird.d.ts
 import * as Bluebird from "bluebird";
 
-declare module "mongoose" {
-    type Promise<T> = Bluebird<T>;
-}
+/*declare module "mongoose" {
+	type Promise<T> = Bluebird<T>;
+}*/
 import * as mongoose from "mongoose";
 
 import * as Data from "../models/Data";
 
 export class DataManager {
 
-    getData(instanceId: String, formName?: String): Bluebird<Data.Document> {
-        let query = Data.model.findOne({ instanceId: instanceId, formName: formName }).exec();
-        return query.then((doc: Data.Document) => {
-            return Bluebird.resolve(doc);
-        });
-    }
+	public getData(instanceId: String, formName?: String): Bluebird<Data.Document> {
+		return new Bluebird<Data.Document>((resolve, reject) => {
+			Data.model.findOne({instanceId, formName}).exec().
+				then((doc: Data.Document) => {
+					if (doc) {
+						resolve(doc);
+					}
+					else {
+						resolve(null);
+					}
+				})
+				.catch((reason: any) => {
+					reject(reason);
+				});
+		});
+	}
 
-    saveData(instanceId: string, formName: string, dataObj: Object): Bluebird<boolean> {
-        // if so, update & save
-        //      return true
-        // else save new doc
-        //      return true
+	public saveData(instanceId: string, formName: string, dataObj: Object): Bluebird<boolean> {
+		// if so, update & save
+		//      return true
+		// else save new doc
+		//      return true
 
-        // query to check if doc exists for <instanceId, formName?>
-        let overallPromise = this.getData(instanceId, formName)
-            .then((doc: Data.Document) => {
-                if (doc) {
-                    doc.data = dataObj;
-                    return doc.save()
-                        .then((doc: Data.Document) => true, (reason: any) => false);
-                }
-                else {
-                    let newData = new Data.model({
-                        data: dataObj,
-                        formName: formName,
-                        instanceId: instanceId
-                    });
-                    return newData.save()
-                        .then((doc: Data.Document) => true, (reason: any) => false);
-                }
-            });
-        return overallPromise;
-    }
+		// query to check if doc exists for <instanceId, formName?>
+		return new Bluebird<boolean>((resolve, reject) => {
+			this.getData(instanceId, formName).
+				then((doc: Data.Document) => {
+					if (doc) {
+						doc.data = dataObj;
+						doc.save().then(
+							(docmt: Data.Document) => resolve(true),
+							(reason: any) => resolve(false),
+						);
+					}
+					else {
+						let newData = new Data.model({
+							data: dataObj,
+							formName,
+							instanceId,
+						});
+						newData.save().then(
+							(docmt: Data.Document) => resolve(true),
+							(reason: any) => resolve(false),
+						);
+					}
+				});
+		});
+	}
 }
