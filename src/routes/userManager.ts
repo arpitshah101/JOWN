43b82c833b641a1b01f2c7e23822163c677f1b4d
Jon <jjgetahun@gmail.com>
2016-12-10 13:56:55 -0500

    Updated functions.

--- src/routes/userManager.ts
+++ src/routes/userManager.ts
@@ -5,7 +5,7 @@ import { UserManager } from "../modules/userManager";
 
 let router = Router();
 
-function createUser(req: Request, rep: Response) {
+function createUser(req: Request, rep: Response, next) {
 	let name = req.params("name");
 	let email = req.params("email");
 	let userId = req.params("userId");
@@ -15,65 +15,69 @@ function createUser(req: Request, rep: Response) {
 
 	let result = UserManager.prototype.createUser(name, email, userId, password, roles, created);
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response === true) {
 			console.log("User " + userId + " created successfully!");
 		}
 		else {
 			console.log("User could not be created!");
 		}
-	});
+	})
+	.catch(next);
 
 }
 
-function deleteUser(req: Request, rep: Response) {
+function deleteUser(req: Request, rep: Response, next) {
 	let userId = req.params("userId");
 
 	let result = UserManager.prototype.deleteUser(userId);
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response === true) {
 			console.log("User " + userId + " deleted successfully!");
 		}
 		else {
 			console.log("User could not be deleted!");
 		}
-	});
+	})
+	.catch(next);
 
 }
 
-function getNextTenUsers(req: Request, rep: Response) {
+function getNextTenUsers(req: Request, rep: Response, next) {
 	let created = req.params("created");
 
 	let result = UserManager.prototype.getNextTenUsers(created);
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response !== null) {
 			console.log("Successfully got the next 1-10 users!");
 		}
 		else {
 			console.log("Could not get the next 1-10 users!");
 		}
-	});
+	})
+	.catch(next);
 
 }
 
-function getUserCount(req: Request, rep: Response) {
+function getUserCount(req: Request, rep: Response, next) {
 
 	let result = UserManager.prototype.getUserCount();
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response > 0) {
 			console.log("There are 1 or more users!");
 		}
 		else {
 			console.log("There are no users!");
 		}
-	});
+	})
+	.catch(next);
 
 }
 
-function modifyUser(req: Request, rep: Response) {
+function modifyUser(req: Request, rep: Response, next) {
 	let email = req.params("email");
 	let userId = req.params("userId");
 	let password = req.params("password");
@@ -81,29 +85,31 @@ function modifyUser(req: Request, rep: Response) {
 
 	let result = UserManager.prototype.modifyUser(userId, email, password, roles);
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response === true) {
 			console.log("User " + userId + " successfully modified!");
 		}
 		else {
 			console.log("User could not be modified!");
 		}
-	});
+	})
+	.catch(next);
 
 }
 
-function userExists(req: Request, rep: Response) {
+function userExists(req: Request, rep: Response, next) {
 	let userId = req.params("userId");
 
 	let result = UserManager.prototype.userExists(userId);
 
-	result.then(function (response) {
+	result.then( (response) => {
 		if (response !== null) {
 			console.log("User " + userId + " exists!");
 		}
 		else {
 			console.log("User does not exist!");
 		}
-	});
+	})
+	.catch(next);
 
 }
