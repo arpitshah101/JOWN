0b6c1294291582e69c382431c8e4fb26248d77be
Arpit Shah <arpitshah101@gmail.com>
2016-12-12 00:05:25 -0500

    created new build task in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,82 +10,83 @@ const watchify = require("watchify");
 const tslint = require("gulp-tslint");
 
 const basePaths = {
-    src: "src/",
-    dest: "dest/",
-    bower: "bower_components/",
-    tests: "src/test/**"
+	bower: "bower_components/",
+	dest: "dest/",
+	src: "src/",
+	tests: "src/test/**",
 };
 
 const exec = require("child_process").exec;
 
 function cleanFolder(folderPath) {
-    exec("rm -r " + folderPath, function (error, stdout, stderr) {
-        if (error) {
-            console.error("exec error:"  + error);
-            return;
-        }
-    });
-
+	exec("rm -r " + folderPath, (error, stdout, stderr) => {
+		if (error) {
+			console.error("exec error:"  + error);
+			return;
+		}
+	});
 }
 
 /*
-    Used to clear the destination folder "dest" before building.
-    If errors are encountered, "dest" will not be completely built.
+	Used to clear the destination folder "dest" before building.
+	If errors are encountered, "dest" will not be completely built.
 */
 gulp.task("clean-dest", function () {
-    cleanFolder(basePaths.dest);
+	cleanFolder(basePaths.dest);
 });
 
 /*
-    Used to compile all .ts files and place them in the corresponding directory structure within "dest" folder
+	Used to compile all .ts files and place them in the corresponding directory structure within "dest" folder
 */
-gulp.task("compile-ts", ['clean-dest'], function () {
-    return tsProject.src()
-        .pipe(tsProject({
-            skipLibCheck: true
-        }))
-        .js.pipe(gulp.dest(basePaths.dest));
+gulp.task("compile-ts", ["clean-dest"], function () {
+	return tsProject.src()
+		.pipe(tsProject({
+			skipLibCheck: true,
+		}))
+		.js.pipe(gulp.dest(basePaths.dest));
 });
 
 /*
-    Copy over public folder & contents without any additional process.
+	Copy over public folder & contents without any additional process.
 */
 gulp.task("copy-public", function () {
-    return gulp.src([basePaths.src + 'public/**'], { base: basePaths.src })
-        .pipe(gulp.dest(basePaths.dest));
+	return gulp.src([basePaths.src + "public/**"], { base: basePaths.src })
+		.pipe(gulp.dest(basePaths.dest));
 });
 
 gulp.task("copy-tests", function () {
-    return gulp.src([basePaths.src + 'test/**'], { base: basePaths.src })
-        .pipe(gulp.dest(basePaths.dest));
+	return gulp.src([basePaths.src + "test/**"], { base: basePaths.src })
+		.pipe(gulp.dest(basePaths.dest));
 });
 
-gulp.task('test', ['copy-tests', 'compile-ts'], function () {
-    return gulp
-        .src("dest/test/*.js")
-        .pipe(mocha({
-            R: 'spec',
-            istanbul: true
-        }));
+gulp.task("test", ["copy-tests", "compile-ts"], function () {
+	return gulp
+		.src("dest/test/*.js")
+		.pipe(mocha({
+			R: "spec",
+			istanbul: true,
+		}));
 });
 
-gulp.task('tslint', () => {
-    return gulp.src(['**/*.ts', '!**/*.d.ts', '!node_modules/**'])
-        .pipe(tslint({
-            formatter: "verbose"
-        }))
-        .pipe(tslint.report({
-            emitError: false
-        }));
+gulp.task("tslint", () => {
+	return gulp.src(["**/*.ts", "!**/*.d.ts", "!node_modules/**"])
+		.pipe(tslint({
+			formatter: "verbose",
+		}))
+		.pipe(tslint.report({
+			emitError: false,
+		}));
 });
 
-gulp.task("default", ['clean-dest', 'compile-ts', 'copy-tests', 'copy-public', 'test'], function () {
+gulp.task("build", ["clean-dest", "compile-ts", "copy-public"]);
 
-    // ----------------- USE FOR WHEN PUBLIC FOLDER IS FINALLY OCCUPIED -----------------
+gulp.task("default", ["clean-dest", "compile-ts", "copy-tests", "copy-public", "test"], function () {
 
-    // gulp.watch('src/public/**', function(event) {
-    //     cleanFolder(basePaths.dest + 'public');
-    //     gulp.run('copy-public');
-    // });
+	// ----------------- USE FOR WHEN PUBLIC FOLDER IS FINALLY OCCUPIED -----------------
+
+	// gulp.watch("src/public/**", function(event) {
+	//     cleanFolder(basePaths.dest + "public");
+	//     gulp.run("copy-public");
+	// });
 
 });
